2e025c21205b5ee0953170ed2a954b6dbca92dd7
nikhilchandurkar <nikhilchandurkar24@gamil.com>
2025-01-14 15:14:15 +0530

    add dockerfile and cicd yml file

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(cookieParser());
 app.use(cors(corsOption));
 
 app.get("/", (req, res) => {
-    res.json("hello its chatAPI");
+    res.json("hello its chat app API");
 });
 
 app.get('/favicon.ico', (req, res) => {
